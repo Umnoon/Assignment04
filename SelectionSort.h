cc04efe4502e01491641d517d20c539bfe4dbb27
Umnoon71 <44784594+Umnoon71@users.noreply.github.com>
2018-12-20 16:04:04 +0600

    Create SelectionSort.h

---
+++ SelectionSort.h
@@ -0,0 +1,10 @@
+#pragma once
+class SelectionSort
+{
+public:
+	SelectionSort();
+	~SelectionSort();
+	void main_selection_sort(int arr[], int size);
+	void main_swap(int *first, int *lowest);
+};
+
